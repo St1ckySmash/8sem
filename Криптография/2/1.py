14c4326db027c000501427fee3895856837e395b
St1ckySmash <99824309+St1ckySmash@users.noreply.github.com>
2025-02-21 00:02:44 +0400

    шота

--- Криптография/2/1.py
+++ Криптография/2/1.py
@@ -3,15 +3,16 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk
 from Crypto.Util.number import getPrime, bytes_to_long, long_to_bytes
-from sympy import isprime, primerange
+from sympy import isprime, primefactors
 import random
 import string
 import math
+from multiprocessing import Pool, cpu_count
 
 ALL_CHARACTERS = (
     "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".lower() + string.digits + string.punctuation
 )
-BIT_LENGTH = 337
+BIT_LENGTH = 338
 
 
 # ДОБАВИТЬ ВЫВОД ПРОМЕЖУТОЧНЫХ РЕЗУЛЬТАТОВ
@@ -73,27 +74,56 @@ def is_prime(n, k=5):
     return True
 
 
-def is_primitive_root(g, p):
-    required_set = {num for num in range(1, p) if gcd(num, p) == 1}
-    actual_set = {pow(g, powers, p) for powers in range(1, p)}
-    return required_set == actual_set
+# def is_primitive_root(g, p):
+# if pow(g, (p - 1) // 2, p) == 1:
+# return False
+# q = (p - 1) // 2
+# if pow(g, q, p) == 1:
+# return False
+# return True
+
+
+def is_primitive_root(g, p, factors, phi):
+    return all(pow(g, phi // factor, p) != 1 for factor in factors)
+
+
+def find_primitive_root(p):
+    phi = p - 1
+    factors = primefactors(phi)
+
+    for g in range(2, min(50, p)):
+        if is_primitive_root(g, p, factors, phi):
+            return g
+    raise Exception("Примитивный корень не найден.")
 
 
 def generate_keys(bit_length=2048):
     while True:
-        p = getPrime(bit_length)
-        if isprime(p):
+        q = getPrime(bit_length - 1)
+        p = 2 * q + 1
+        # if isprime(p):
+        if is_prime(p):
             break
 
-    while True:
-        g = random.randint(2, p - 2)
-        if is_primitive_root(g, p):
-            break
-
-    x = random.randint(1, p - 2)
+    g = find_primitive_root(p)
+    x = random.randint(2, p - 2)
     y = pow(g, x, p)
 
+    print(f"Сгенерированные ключи:\n p = {p}\n g = {g}\n y = {y}\n x = {x}")
+
     return (p, g, y), x
+
+
+###
+# def find_primitive_root(p):
+# if not isprime(p):
+# raise ValueError("Число должно быть простым.")
+# phi = p - 1
+# factors = primefactors(phi)
+# for g in range(2, p):
+# if all(pow(g, phi // factor, p) != 1 for factor in factors):
+# return g
+# raise Exception("Примитивный корень не найден.")
 
 
 # Функция для вычисления НОД
@@ -294,178 +324,178 @@ def generate_keys_action():
     log_text.insert(tk.END, "'public_key.txt' и 'private_key.txt'\n")
 
 
-root = tk.Tk()
-root.title("Шифрование и Расшифровка")
-root.geometry("1300x560")
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Шифрование и Расшифровка")
+    root.geometry("1300x560")
 
-style = ttk.Style()
-style.configure(
-    "TButton",
-    padding=6,
-    relief="flat",
-    background="#005f73",
-    foreground="blue",
-    font=("Helvetica", 10, "bold"),
-)
-style.map(
-    "TButton",
-    foreground=[("disabled", "#aaaaaa"), ("active", "white")],
-    background=[("disabled", "#d3d3d3"), ("active", "#002f3d")],
-)
-style.configure(
-    "TLabel",
-    padding=6,
-    relief="flat",
-    background="#0a9396",
-    foreground="white",
-    font=("Helvetica", 10),
-)
-style.configure("TFrame", padding=6, relief="flat", background="#94d2bd")
+    style = ttk.Style()
+    style.configure(
+        "TButton",
+        padding=6,
+        relief="flat",
+        background="#005f73",
+        foreground="blue",
+        font=("Helvetica", 10, "bold"),
+    )
+    style.map(
+        "TButton",
+        foreground=[("disabled", "#aaaaaa"), ("active", "white")],
+        background=[("disabled", "#d3d3d3"), ("active", "#002f3d")],
+    )
+    style.configure(
+        "TLabel",
+        padding=6,
+        relief="flat",
+        background="#0a9396",
+        foreground="white",
+        font=("Helvetica", 10),
+    )
+    style.configure("TFrame", padding=6, relief="flat", background="#94d2bd")
 
-frame = ttk.Frame(root)
-frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+    frame = ttk.Frame(root)
+    frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-left_frame = ttk.Frame(frame)
-left_frame.grid(row=0, column=0, sticky="nw")
+    left_frame = ttk.Frame(frame)
+    left_frame.grid(row=0, column=0, sticky="nw")
 
-right_frame = ttk.Frame(frame)
-right_frame.grid(row=0, column=1, sticky="nw")
+    right_frame = ttk.Frame(frame)
+    right_frame.grid(row=0, column=1, sticky="nw")
 
-buttons_frame = ttk.Frame(frame)
-# buttons_frame.grid(row=0, column №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
-buttons_frame.grid(row=0, column=2, sticky="nw")
+    buttons_frame = ttk.Frame(frame)
+    # buttons_frame.grid(row=0, column №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
+    buttons_frame.grid(row=0, column=2, sticky="nw")
 
-open_text_label = ttk.Label(left_frame, text="Выберите файл с открытым текстом")
-open_text_label.pack(anchor="w", pady=5)
-open_text_button = ttk.Button(
-    left_frame,
-    text="Выбрать файл",
-    command=lambda: select_file(open_text_path_label),
-)
-open_text_button.pack(anchor="w")
-open_text_path_label = ttk.Label(left_frame, text="")
-open_text_path_label.pack(anchor="w", pady=2)
+    open_text_label = ttk.Label(left_frame, text="Выберите файл с открытым текстом")
+    open_text_label.pack(anchor="w", pady=5)
+    open_text_button = ttk.Button(
+        left_frame,
+        text="Выбрать файл",
+        command=lambda: select_file(open_text_path_label),
+    )
+    open_text_button.pack(anchor="w")
+    open_text_path_label = ttk.Label(left_frame, text="")
+    open_text_path_label.pack(anchor="w", pady=2)
 
-cipher_text_label = ttk.Label(left_frame, text="Выберите файл с шифротекстом")
-cipher_text_label.pack(anchor="w", pady=5)
-cipher_text_button = ttk.Button(
-    left_frame,
-    text="Выбрать файл",
-    command=lambda: select_file(cipher_text_path_label),
-)
-cipher_text_button.pack(anchor="w")
-cipher_text_path_label = ttk.Label(left_frame, text="")
-cipher_text_path_label.pack(anchor="w", pady=2)
+    cipher_text_label = ttk.Label(left_frame, text="Выберите файл с шифротекстом")
+    cipher_text_label.pack(anchor="w", pady=5)
+    cipher_text_button = ttk.Button(
+        left_frame,
+        text="Выбрать файл",
+        command=lambda: select_file(cipher_text_path_label),
+    )
+    cipher_text_button.pack(anchor="w")
+    cipher_text_path_label = ttk.Label(left_frame, text="")
+    cipher_text_path_label.pack(anchor="w", pady=2)
 
-key_label_open = ttk.Label(left_frame, text="Выберите файл с открытым ключом")
-key_label_open.pack(anchor="w", pady=5)
-key_button_open = ttk.Button(
-    left_frame,
-    text="Выбрать файл",
-    command=lambda: select_file(key_path_open_label),
-)
-key_button_open.pack(anchor="w")
-key_path_open_label = ttk.Label(left_frame, text="")
-key_path_open_label.pack(anchor="w", pady=2)
+    key_label_open = ttk.Label(left_frame, text="Выберите файл с открытым ключом")
+    key_label_open.pack(anchor="w", pady=5)
+    key_button_open = ttk.Button(
+        left_frame,
+        text="Выбрать файл",
+        command=lambda: select_file(key_path_open_label),
+    )
+    key_button_open.pack(anchor="w")
+    key_path_open_label = ttk.Label(left_frame, text="")
+    key_path_open_label.pack(anchor="w", pady=2)
 
-key_label_private = ttk.Label(left_frame, text="Выберите файл с приватным ключом")
-key_label_private.pack(anchor="w", pady=5)
-key_button_private = ttk.Button(
-    left_frame,
-    text="Выбрать файл",
-    command=lambda: select_file(key_path_private_label),
-)
-key_button_private.pack(anchor="w")
-key_path_private_label = ttk.Label(left_frame, text="")
-key_path_private_label.pack(anchor="w", pady=2)
+    key_label_private = ttk.Label(left_frame, text="Выберите файл с приватным ключом")
+    key_label_private.pack(anchor="w", pady=5)
+    key_button_private = ttk.Button(
+        left_frame,
+        text="Выбрать файл",
+        command=lambda: select_file(key_path_private_label),
+    )
+    key_button_private.pack(anchor="w")
+    key_path_private_label = ttk.Label(left_frame, text="")
+    key_path_private_label.pack(anchor="w", pady=2)
 
-save_encrypt_file_label = ttk.Label(
-    right_frame, text="Выберите файл для сохранения зашифрованного текста"
-)
-save_encrypt_file_label.pack(anchor="w", pady=5)
-save_encrypt_file_button = ttk.Button(
-    right_frame,
-    text="Выбрать файл",
-    command=lambda: select_file(save_encrypt_path_label),
-)
-save_encrypt_file_button.pack(anchor="w")
-save_encrypt_path_label = ttk.Label(right_frame, text="")
-save_encrypt_path_label.pack(anchor="w", pady=2)
+    save_encrypt_file_label = ttk.Label(
+        right_frame, text="Выберите файл для сохранения зашифрованного текста"
+    )
+    save_encrypt_file_label.pack(anchor="w", pady=5)
+    save_encrypt_file_button = ttk.Button(
+        right_frame,
+        text="Выбрать файл",
+        command=lambda: select_file(save_encrypt_path_label),
+    )
+    save_encrypt_file_button.pack(anchor="w")
+    save_encrypt_path_label = ttk.Label(right_frame, text="")
+    save_encrypt_path_label.pack(anchor="w", pady=2)
 
-save_decrypt_file_label = ttk.Label(
-    right_frame, text="Выберите файл для сохранения расшифрованного текста"
-)
-save_decrypt_file_label.pack(anchor="w", pady=5)
-save_decrypt_file_button = ttk.Button(
-    right_frame,
-    text="Выбрать файл",
-    command=lambda: select_file(save_decrypt_path_label),
-)
-save_decrypt_file_button.pack(anchor="w")
-save_decrypt_path_label = ttk.Label(right_frame, text="")
-save_decrypt_path_label.pack(anchor="w", pady=2)
+    save_decrypt_file_label = ttk.Label(
+        right_frame, text="Выберите файл для сохранения расшифрованного текста"
+    )
+    save_decrypt_file_label.pack(anchor="w", pady=5)
+    save_decrypt_file_button = ttk.Button(
+        right_frame,
+        text="Выбрать файл",
+        command=lambda: select_file(save_decrypt_path_label),
+    )
+    save_decrypt_file_button.pack(anchor="w")
+    save_decrypt_path_label = ttk.Label(right_frame, text="")
+    save_decrypt_path_label.pack(anchor="w", pady=2)
 
-result_label = ttk.Label(right_frame, text="Результат:")
-result_label.pack(anchor="w", pady=5)
-result_text = tk.Text(right_frame, height=2, width=50, bg="#e9d8a6", fg="#001219")
-result_text.pack(anchor="w", padx=10, pady=10)
+    result_label = ttk.Label(right_frame, text="Результат:")
+    result_label.pack(anchor="w", pady=5)
+    result_text = tk.Text(right_frame, height=2, width=50, bg="#e9d8a6", fg="#001219")
+    result_text.pack(anchor="w", padx=10, pady=10)
 
-result_text_context_menu = tk.Menu(result_text, tearoff=0)
-result_text_context_menu.add_command(
-    label="Скопировать",
-    command=lambda: root.clipboard_append(result_text.get(1.0, tk.END)),
-)
-result_text.bind(
-    "<Button-3>",
-    lambda event: result_text_context_menu.post(event.x_root, event.y_root),
-)
+    result_text_context_menu = tk.Menu(result_text, tearoff=0)
+    result_text_context_menu.add_command(
+        label="Скопировать",
+        command=lambda: root.clipboard_append(result_text.get(1.0, tk.END)),
+    )
+    result_text.bind(
+        "<Button-3>",
+        lambda event: result_text_context_menu.post(event.x_root, event.y_root),
+    )
 
-log_text = tk.Text(right_frame, height=12, width=50, bg="#f0e68c", fg="#001219")
-log_text.pack(anchor="w", padx=10, pady=10)
+    log_text = tk.Text(right_frame, height=12, width=50, bg="#f0e68c", fg="#001219")
+    log_text.pack(anchor="w", padx=10, pady=10)
 
-log_text_context_menu = tk.Menu(log_text, tearoff=0)
-log_text_context_menu.add_command(
-    label="Скопировать",
-    command=lambda: root.clipboard_append(log_text.get(1.0, tk.END)),
-)
-log_text.bind(
-    "<Button-3>", lambda event: log_text_context_menu.post(event.x_root, event.y_root)
-)
+    log_text_context_menu = tk.Menu(log_text, tearoff=0)
+    log_text_context_menu.add_command(
+        label="Скопировать",
+        command=lambda: root.clipboard_append(log_text.get(1.0, tk.END)),
+    )
+    log_text.bind(
+        "<Button-3>",
+        lambda event: log_text_context_menu.post(event.x_root, event.y_root),
+    )
 
+    class TextRedirector(object):
+        def __init__(self, widget, tag="stdout"):
+            self.widget = widget
+            self.tag = tag
 
-class TextRedirector(object):
-    def __init__(self, widget, tag="stdout"):
-        self.widget = widget
-        self.tag = tag
+        def write(self, str):
+            self.widget.insert(tk.END, str)
+            self.widget.see(tk.END)
 
-    def write(self, str):
-        self.widget.insert(tk.END, str)
-        self.widget.see(tk.END)
+        def flush(self):
+            pass
 
-    def flush(self):
-        pass
+    import sys
 
+    sys.stdout = TextRedirector(log_text)
 
-import sys
+    generate_keys_button = ttk.Button(
+        buttons_frame,
+        text="Сгенерировать ключи",
+        command=generate_keys_action,
+        state=tk.DISABLED,
+    )
+    generate_keys_button.pack(anchor="w", pady=10)
 
-sys.stdout = TextRedirector(log_text)
+    encrypt_button = ttk.Button(
+        buttons_frame, text="Зашифровать", command=encrypt_text, state=tk.DISABLED
+    )
+    encrypt_button.pack(anchor="w", pady=10)
 
-generate_keys_button = ttk.Button(
-    buttons_frame,
-    text="Сгенерировать ключи",
-    command=generate_keys_action,
-    state=tk.DISABLED,
-)
-generate_keys_button.pack(anchor="w", pady=10)
+    decrypt_button = ttk.Button(
+        buttons_frame, text="Расшифровать", command=decrypt_text, state=tk.DISABLED
+    )
+    decrypt_button.pack(anchor="w", pady=10)
 
-encrypt_button = ttk.Button(
-    buttons_frame, text="Зашифровать", command=encrypt_text, state=tk.DISABLED
-)
-encrypt_button.pack(anchor="w", pady=10)
-
-decrypt_button = ttk.Button(
-    buttons_frame, text="Расшифровать", command=decrypt_text, state=tk.DISABLED
-)
-decrypt_button.pack(anchor="w", pady=10)
-
-root.mainloop()
+    root.mainloop()
